8b85135617930054def8fc81de34ad7d32181147
wrottger <wrottger@student.42wolfsburg.de>
2024-04-12 13:43:40 +0200

    Add: http errors

--- src/HttpError_test.cpp
+++ src/HttpError_test.cpp
@@ -1,9 +1,30 @@
 #include "HttpError.hpp"
 #include "utest.h"
 
-UTEST(HttpError, minimal) {
+UTEST(HttpError, minimal)
+{
   HttpError error(404, "Not Found");
   ASSERT_STREQ("Not Found", error.what());
+  int code = error.code();
+  ASSERT_EQ(404, code);
+}
+
+UTEST(HttpError, throwing)
+{
+  HttpError error(404, "Not Found");
+  try {
+    throw error;
+  } catch (HttpError& e) {
+    ASSERT_STREQ("Not Found", e.what());
+    int code = e.code();
+    ASSERT_EQ(404, code);
+  }
+}
+
+UTEST(HttpError, empty)
+{
+  HttpError error(0, NULL);
+  ASSERT_STREQ("", error.what());
   int code = error.code();
   ASSERT_EQ(404, code);
 }
