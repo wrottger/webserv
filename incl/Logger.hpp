aea1d839c56bb0931f2c5208837b7ab7e4581fd3
stevebalk <steve.balk@web.de>
2024-04-22 13:16:31 +0200

    Added logger class

---
+++ incl/Logger.hpp
@@ -0,0 +1,152 @@
+#ifndef LOGGING_HPP
+#define LOGGING_HPP
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include <iostream>
+#include <ctime>
+#include <sys/time.h>
+
+/* The Logger class is implement as a singleton class that clears itself after the program ends. */
+/* So you don't need to worry about cleaning up the logger object. */
+
+namespace Logging {
+	// Log file name
+	static const std::string LOG_SAVE_FILE = "log.txt";
+
+	// Direct interface for logging using a MACRO
+	#define LOG_ERROR(text)		Logging::Logger::getInstance().error(text)	// log error message
+	#define LOG_ALARM(text)		Logging::Logger::getInstance().alarm(text)	// log alarm message
+	#define LOG_ALWAYS(text)	Logging::Logger::getInstance().always(text)	// log always message
+	#define LOG_BUFFER(text)	Logging::Logger::getInstance().buffer(text)	// log RAW buffer message
+	#define LOG_INFO(text)		Logging::Logger::getInstance().info(text)	// log info message
+	#define LOG_TRACE(text)		Logging::Logger::getInstance().trace(text)	// log trace message
+	#define LOG_DEBUG(text)		Logging::Logger::getInstance().debug(text)	// log debug message
+
+	// Interfaces to control log levels and log types
+	#define LOG_SET_LOG_LEVEL(level)	Logging::Logger::getInstance().setLogLevel(level)	// Set log level
+	#define LOG_SET_LOG_TARGET(type)	Logging::Logger::getInstance().setLogTarget(type)	// Set log target
+
+	// Interfaces to enable and disable logging
+	#define LOG_ENABLE_ALL_LEVELS()		Logging::Logger::getInstance().enableAllLevels()	// Enable all log levels
+	#define LOG_DISABLE_ALL_LEVELS()	Logging::Logger::getInstance().disableAllLevels()	// Disable all log levels, except error and alarm
+
+	// Interfaces to enable and disable console and file logging
+	#define LOG_ENABLE_CONSOLE_LOGGING()	Logging::Logger::getInstance().enableConsoleLogging()	// Enable console logging
+	#define LOG_ENABLE_FILE_LOGGING()		Logging::Logger::getInstance().enableFileLogging()		// Enable file logging
+	#define LOG_DISABLE_CONSOLE_LOGGING()	Logging::Logger::getInstance().disableConsoleLogging()	// Disable console logging
+	#define LOG_DISABLE_FILE_LOGGING()		Logging::Logger::getInstance().disableFileLogging()		// Disable file logging
+	#define LOG_ENABLE_DUO_LOGGING()		Logging::Logger::getInstance().enableDuoLogging()		// Enable both console and file logging
+
+	// Interfaces to enable and disable time stamp and log level in log
+	#define LOG_ENABLE_PRINT_TIME_STAMP()	Logging::Logger::getInstance().enablePrintTimeStamp()	// Enable time stamp in log
+	#define LOG_DISABLE_PRINT_TIME_STAMP()	Logging::Logger::getInstance().disablePrintTimeStamp()	// Disable time stamp in log
+	#define LOG_ENABLE_PRINT_LOG_LEVEL()	Logging::Logger::getInstance().enablePrintLogLevel()	// Enable log level in log
+	#define LOG_DISABLE_PRINT_LOG_LEVEL()	Logging::Logger::getInstance().disablePrintLogLevel()	// Disable log level in log
+
+	typedef enum LOG_LEVEL {
+		DISABLE_LOG,
+		LOG_LVL_INFO,
+		LOG_LVL_BUFFER,
+		LOG_LVL_TRACE,
+		LOG_LVL_DEBUG,
+		ENABLE_LOG
+	} LogLevel;
+
+	typedef enum LOG_TARGET {
+		LOG_NONE = 0,
+		LOG_TO_CONSOLE = 1,
+		LOG_TO_FILE = 2,
+		LOG_TO_BOTH = 3
+	} LogTarget;
+
+	class Logger {
+	public:
+		static Logger& getInstance(); // Return the instance of the Logger
+
+		void error(const char* text);			// log error message
+		void error(std::string& text);			// log error message
+		void error(std::ostringstream& stream);	// log error message
+
+		void alarm(const char* text);			// log alarm message
+		void alarm(std::string& text);			// log alarm message
+		void alarm(std::ostringstream& stream); // log alarm message
+
+		void always(const char* text);			// log always message
+		void always(std::string& text);			// log always message
+		void always(std::ostringstream& stream);// log always message
+
+		void buffer(const char* text);			// log RAW buffer message
+		void buffer(std::string& text);			// log RAW buffer message
+		void buffer(std::ostringstream& stream);// log RAW buffer message
+
+		void info(const char* text);			// log info message
+		void info(std::string& text);			// log info message
+		void info(std::ostringstream& stream);	// log info message
+
+		void trace(const char* text);			// log trace message
+		void trace(std::string& text);			// log trace message
+		void trace(std::ostringstream& stream);	// log trace message
+
+		void debug(const char* text);			// log debug message
+		void debug(std::string& text);			// log debug message
+		void debug(std::ostringstream& stream);	// log debug message
+
+		// Error and Alarm log are always enabled
+		// Hence, there is no interfce to control error and alarm logs
+
+		/* Handle log levels */
+
+		void setLogLevel(LogLevel logLevel); // Set log level
+		void enableAllLevels();					// Enable all log levels
+		void disableAllLevels();				// Disable all log levels, except error and alarm
+
+		void setLogTarget(LogTarget logTarget); // Set log type
+
+		/* Handle where log output is send to */
+
+		void enableConsoleLogging();	// Enable console logging
+		void enableFileLogging();		// Enable file logging
+		void enableDuoLogging();		// Enable both console and file logging
+
+		void disableConsoleLogging();	// Disable console logging
+		void disableFileLogging();		// Disable file logging
+		void disableLogging();			// Disable both console and file logging
+
+		/* Handle what is printed in log */
+
+		void enablePrintTimeStamp();	// Enable time stamp in log
+		void enablePrintLogLevel();		// Enable log level in log
+
+		void disablePrintTimeStamp();	// Disable time stamp in log
+		void disablePrintLogLevel();	// Disable log level in log
+
+	protected:
+		Logger();
+		~Logger();
+
+
+	private:
+		static Logger* _instance;
+		std::ofstream _logFile;
+		LogLevel _logLevel;
+		LogTarget _logTarget;
+		bool _timeStampInLog;
+		bool _logLevelInLog;
+
+	private:
+		std::string getCurrentTime();
+		void logMessage(const char* level, const char* text);
+		void createLogMessage(std::string &buffer, const char *logLevel, const char *text);
+		void writeLog(const std::string &data);
+		void startLogging();
+		std::string insertMetaInformations(const char *logLevel);
+
+		Logger(const Logger& other);				// Copy constructor
+		Logger& operator=(const Logger& other);		// Assignment operator
+	};
+}
+
+#endif // LOGGING_HPP
---
+++ src/Logger.cpp
@@ -0,0 +1,311 @@
+#include "Logger.hpp"
+
+Logging::Logger *Logging::Logger::_instance = NULL;
+
+Logging::Logger::Logger() {
+	_logFile.open(LOG_SAVE_FILE.c_str(), std::ios::out | std::ios::app);
+	if (_logFile.fail()) {
+		std::cerr << "Failed to open log file: " << LOG_SAVE_FILE << std::endl;
+	}
+	_logLevel = LOG_LVL_DEBUG;		// Default log level
+	_logTarget = LOG_TO_CONSOLE;	// Default log type
+	_timeStampInLog = true;			// Default timestamp in log
+	_logLevelInLog = true;			// Default log level in log
+	startLogging();
+}
+
+Logging::Logger::~Logger() {
+	_logFile.close();
+}
+
+// Singleton instance
+Logging::Logger &Logging::Logger::getInstance() {
+	static Logger _instance;
+	return _instance;
+}
+
+void Logging::Logger::startLogging() {
+	buffer("************************************************\n");
+	buffer("*   Logging started: ");
+	buffer(getCurrentTime().c_str());
+	buffer("   *\n");
+	buffer("************************************************\n");
+}
+
+std::string Logging::Logger::getCurrentTime() {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+
+	std::time_t rawtime = tv.tv_sec;
+	std::tm *timeinfo = std::localtime(&rawtime);
+
+	char buffer[80];
+	std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", timeinfo);
+
+	std::ostringstream oss;
+	oss << buffer << "." << std::setfill('0') << std::setw(3) << tv.tv_usec / 1000;
+
+	return oss.str();
+}
+
+// Creates and writes log message
+void Logging::Logger::logMessage(const char* level, const char* text) {
+	std::string logline;
+	createLogMessage(logline, level, text);
+	writeLog(logline);
+}
+
+// Creates log message
+void Logging::Logger::createLogMessage(std::string &buffer, const char *logLevel, const char *text) {
+	buffer.append(insertMetaInformations(logLevel));
+	buffer.append(text);
+}
+
+// Writes log message
+void Logging::Logger::writeLog(const std::string &data) {
+	// Write log into file
+	if (_logFile.rdstate() == std::ios_base::goodbit) {
+		if (LOG_TO_FILE == static_cast<LogTarget>(_logTarget & LOG_TO_FILE)) {
+			_logFile << data << std::endl;
+		}
+	}
+	// Write log into console
+	if (LOG_TO_CONSOLE == static_cast<LogTarget>(_logTarget & LOG_TO_CONSOLE)) {
+		std::cout << data << std::endl;
+	}
+}
+
+// Inserts timestamp and log level in log
+std::string Logging::Logger::insertMetaInformations(const char *logLevel) {
+	std::string data;
+
+	if (_timeStampInLog) {
+		data.append(getCurrentTime()).append(" ");
+	}
+
+	if (_logLevelInLog) {
+		data.append("[").append(logLevel).append("]").append(": ");
+	}
+
+	return data;
+}
+
+// log error message
+void Logging::Logger::error(const char *text) {
+	if (_logTarget == LOG_NONE) {
+		return;
+	}
+	// ERROR must be captured
+	logMessage("ERROR", text);
+}
+
+// log error message
+void Logging::Logger::error(std::string &text) {
+	error(text.data());
+}
+
+// log error message
+void Logging::Logger::error(std::ostringstream &stream) {
+	std::string text = stream.str();
+	error(text.data());
+}
+
+// log alarm message
+void Logging::Logger::alarm(const char *text) {
+	if (_logTarget == LOG_NONE) {
+		return;
+	}
+	// ALARM must be captured
+	logMessage("ALARM", text);
+}
+
+// log alarm message
+void Logging::Logger::alarm(std::string &text) {
+	alarm(text.data());
+}
+
+// log alarm message
+void Logging::Logger::alarm(std::ostringstream &stream) {
+	std::string text = stream.str();
+	alarm(text.data());
+}
+
+// log always message
+void Logging::Logger::always(const char *text) {
+	if (_logTarget == LOG_NONE) {
+		return;
+	}
+	// ALWAYS must be captured
+	logMessage("ALWAYS", text);
+}
+
+// log always message
+void Logging::Logger::always(std::string &text) {
+	always(text.data());
+}
+
+// log always message
+void Logging::Logger::always(std::ostringstream &stream) {
+	std::string text = stream.str();
+	always(text.data());
+}
+
+// log RAW buffer message
+void Logging::Logger::buffer(const char *text) {
+	// Buffer is the special case. So don't add log level
+	// and timestamp in the buffer message. Just log the raw bytes.
+	if (_logTarget == LOG_NONE) {
+		return;
+	}
+	if (LOG_TO_FILE == static_cast<LogTarget>(_logTarget & LOG_TO_FILE)) {
+		_logFile << text;
+	}
+	// Write log into console
+	if (LOG_TO_CONSOLE == static_cast<LogTarget>(_logTarget & LOG_TO_CONSOLE)) {
+		std::cout << text;
+	}
+}
+
+// log RAW buffer message
+void Logging::Logger::buffer(std::string &text) {
+	buffer(text.data());
+}
+
+// log RAW buffer message
+void Logging::Logger::buffer(std::ostringstream &stream) {
+	std::string text = stream.str();
+	buffer(text.data());
+}
+
+// log info message
+void Logging::Logger::info(const char *text) {
+	if (_logTarget == LOG_NONE) {
+		return;
+	}
+	if (_logLevel >= LOG_LVL_INFO) {
+		logMessage("INFO", text);
+	}
+}
+
+// log info message
+void Logging::Logger::info(std::string &text) {
+	info(text.data());
+}
+
+// log info message
+void Logging::Logger::info(std::ostringstream &stream) {
+	std::string text = stream.str();
+	info(text.data());
+}
+
+// log trace message
+void Logging::Logger::trace(const char *text) {
+	if (_logTarget == LOG_NONE) {
+		return;
+	}
+	if (_logLevel >= LOG_LVL_TRACE) {
+		logMessage("TRACE", text);
+	}
+}
+
+// log trace message
+void Logging::Logger::trace(std::string &text) {
+	trace(text.data());
+}
+
+// log trace message
+void Logging::Logger::trace(std::ostringstream &stream) {
+	std::string text = stream.str();
+	trace(text.data());
+}
+
+// log debug message
+void Logging::Logger::debug(const char *text) {
+	if (_logTarget == LOG_NONE) {
+		return;
+	}
+	if (_logLevel >= LOG_LVL_DEBUG) {
+		logMessage("DEBUG", text);
+	}
+}
+
+// log debug message
+void Logging::Logger::debug(std::string &text) {
+	debug(text.data());
+}
+
+// log debug message
+void Logging::Logger::debug(std::ostringstream &stream) {
+	std::string text = stream.str();
+	debug(text.data());
+}
+
+// Set log level
+void Logging::Logger::setLogLevel(LogLevel logLevel) {
+	_logLevel = logLevel;
+}
+
+// Enable all log levels
+void Logging::Logger::enableAllLevels() {
+	_logLevel = ENABLE_LOG;
+}
+
+// Disable all log levels, except error and alarm
+void Logging::Logger::disableAllLevels() {
+	_logLevel = DISABLE_LOG;
+}
+
+// Interfaces to control log Types
+void Logging::Logger::setLogTarget(LogTarget logType) {
+	_logTarget = static_cast<LogTarget>(_logTarget | logType);
+}
+
+// Enable file logging
+void Logging::Logger::enableFileLogging() {
+	_logTarget = static_cast<LogTarget>(_logTarget | LOG_TO_FILE);
+}
+
+// Enable console logging
+void Logging::Logger::enableConsoleLogging() {
+	_logTarget = static_cast<LogTarget>(_logTarget | LOG_TO_CONSOLE);
+}
+
+// Disable file logging
+void Logging::Logger::disableFileLogging() {
+	_logTarget = static_cast<LogTarget>(_logTarget & ~LOG_TO_FILE);
+}
+
+// Disable console logging
+void Logging::Logger::disableConsoleLogging() {
+	_logTarget = static_cast<LogTarget>(_logTarget & ~LOG_TO_CONSOLE);
+}
+
+// Enable both file and console logging
+void Logging::Logger::enableDuoLogging() {
+	_logTarget = LOG_TO_BOTH;
+}
+
+// Disable both file and console logging
+void Logging::Logger::disableLogging() {
+	_logTarget = LOG_NONE;
+}
+
+// Show timestamp in log
+void Logging::Logger::enablePrintTimeStamp() {
+	_timeStampInLog = true;
+}
+
+// Hide timestamp in log
+void Logging::Logger::disablePrintTimeStamp() {
+	_timeStampInLog = false;
+}
+
+// Show log level in log
+void Logging::Logger::enablePrintLogLevel() {
+	_logLevelInLog = true;
+}
+
+// Hide log level in log
+void Logging::Logger::disablePrintLogLevel() {
+	_logLevelInLog = false;
+}
