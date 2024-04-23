6201a6f73843e706e92f3bff97d4dea04212e8fe
wrottger <wrottger@student.42wolfsburg.de>
2024-04-23 14:20:48 +0200

    feature: percent decoding

--- src/HttpRequest.cpp
+++ src/HttpRequest.cpp
@@ -7,7 +7,6 @@
 #include "HttpMessage.hpp"
 
 HttpRequest::HttpRequest() : parseError(0, ""){
-    headerComplete = false;
     complete = false;
     state = new StateHandler();
     state->func = &States::method;
@@ -30,7 +29,7 @@ size_t HttpRequest::parseBuffer(const char *requestLine) {
         c = requestLine[i];
         try
         {
-            state->func(c, message, *state);  
+            state->func(c, message, *state);
         }
         catch(HttpError &e)
         {
@@ -39,7 +38,8 @@ size_t HttpRequest::parseBuffer(const char *requestLine) {
         }
         if (state->func == States::headerFinished)
         {
-            headerComplete = true;
+            message.path = percentDecode(message.path);
+            complete = true;
             if (headers.count("host") == 0)
             {
                 parseError = HttpError(400, "Host header is required");
@@ -47,7 +47,6 @@ size_t HttpRequest::parseBuffer(const char *requestLine) {
             }
         }
     }
-    // TODO percent decode path
     return i;
 }
 
@@ -61,7 +60,32 @@ const std::string &HttpRequest::getHeader(const std::string &name) const {
     return message.headers.find(name)->second;
 }
 
+bool HttpRequest::isError() const { return parseError.code() != 0;}
 HttpError HttpRequest::getError() const { return parseError; }
+
+std::string HttpRequest::percentDecode(std::string &str)
+{
+    std::string decoded;
+    for (size_t i = 0; i < str.size(); i++)
+    {
+        if (str[i] == '%')
+        {
+            if (i + 2 >= str.size())
+            {
+                parseError = HttpError(400, "Invalid percent encoding");
+                return "";
+            }
+            char c = (str[i + 1] - '0') * 16 + (str[i + 2] - '0');
+            decoded.push_back(c);
+            i += 2;
+        }
+        else
+        {
+            decoded.push_back(str[i]);
+        }
+    }
+    return decoded;
+}
 
 bool HttpRequest::isComplete() const { return state->func == States::headerFinished; }
 
