bbf84d5219f6d52c6760f62b221cdd51b352c23d
wrottger <wrottger@student.42wolfsburg.de>
2024-04-25 11:07:32 +0200

    First attempt at a HttpResponse class

---
+++ incl/HttpResponse.hpp
@@ -0,0 +1,20 @@
+#ifndef HTTPRESPONSE_HPP
+#define HTTPRESPONSE_HPP
+
+#include "HttpHeader.hpp"
+
+class HttpResponse
+{
+    public:
+        HttpResponse(HttpHeader &header, int fds);
+        ~HttpResponse();
+        size_t readBuffer(const char* buffer);
+        void canWrite(bool canWrite);
+        bool finished();
+
+    private:
+        HttpResponse();
+        HttpResponse(const HttpResponse &src);
+        HttpResponse &operator=(const HttpResponse &src);
+};
+#endif // HTTPRESPONSE_HPP
