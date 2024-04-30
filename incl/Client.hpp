3dc3a2603caa9f5373833f8959517b6152982bde
stevebalk <steve.balk@web.de>
2024-04-30 16:21:49 +0200

    Added new getter functions and renamed variable

--- incl/Client.hpp
+++ incl/Client.hpp
@@ -2,8 +2,8 @@
 #define CLIENT_HPP
 
 #include "HttpHeader.hpp"
-#include <ctime>
 #include <unistd.h>
+#include <ctime>
 
 class EventHandler;
 
@@ -11,18 +11,20 @@ class Client {
 public:
 	Client(int fd, EventHandler *eventHandler);
 	~Client();
-	int getFd();
-	std::time_t getLastModified();
+	int getFd() const;
+	std::time_t getLastModified() const;
 	void updateTime();
-	bool isHeaderComplete();
-	bool isBodyComplete();
+	bool isHeaderComplete() const;
+	bool isBodyComplete() const;
 	void parseBuffer(const char *buffer);
+	EventHandler *getEventHandler() const;
+	HttpHeader *getHeaderObject() const;
 
 private:
 	Client();
 	Client(Client const &other);
 	Client &operator=(Client const &other);
-	HttpHeader *_requestObject;
+	HttpHeader *_headerObject;
 	int _fd;
 	std::time_t _lastModified;
 	EventHandler *_eventHandler;
