009bdd08c8fc90da3353b7ec3f1c318179ae9244
stevebalk <steve.balk@web.de>
2024-04-30 16:20:56 +0200

    Renamed function and included Client.hpp

--- incl/Cgi.hpp
+++ incl/Cgi.hpp
@@ -1,6 +1,7 @@
 #ifndef CGI_HPP
 #define CGI_HPP
 
+#include "Client.hpp"
 #include "Config.hpp"
 #include "EventHandler.hpp"
 #include "Logger.hpp"
@@ -26,8 +27,8 @@ private:
 	Cgi(const Cgi &other);
 	Cgi &operator=(const Cgi &other);
 	const char *createEnviroment();
-	void executeCgi(const std::string &bodyBuffer, const int &epollFd, const HttpHeader &requestObject);
-	int excecuteChild(const std::string &bodyBuffer, const HttpHeader &requestObject);
+	void executeCgi(const std::string &bodyBuffer, Client *client);
+	int executeChild(const std::string &bodyBuffer, const HttpHeader *headerObject);
 
 public:
 	Cgi(const std::string &bodyBuffer, Client *client);
