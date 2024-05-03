ac003bf02c917f4172096a0620fc31b429a400df
wrottger <wrottger@student.42wolfsburg.de>
2024-05-03 16:04:23 +0200

    First iteration of input handling

--- src/HttpResponse.cpp
+++ src/HttpResponse.cpp
@@ -3,24 +3,58 @@
 #include <fstream>
 #include <iostream>
 #include <sys/stat.h>
+#include "Logger.hpp"
 #include "Config.hpp"
 
+std::string HttpResponse::generateErrorResponse(int code, const std::string &message) {
+	std::string error_path = config->getErrorPage(error.code(), header.getPath(), header.getHeader("host"));
+	response = "HTTP/1.1 ";
+	if (error_path.empty())
+	{
+		response += code + " ";
+		response += message + "\r\n";
+		response += "Connection: close\r\n\r\n";
+	}
+	else
+	{
+		std::ifstream file(config->getFilePath(error_path, header.getHeader("host")).c_str());
+		if (!file.is_open())
+		{
+			response += "HTTP/1.1 500 Internal Server Error\r\n";
+			response += "Connection: close\r\n\r\n";
+			return response;
+		}
+		response += code + " ";
+		response += message + "\r\n";
+		response += "Connection: close\r\n";
+		response += "Content-Type: text/html\r\n\r\n";
+		response += std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+	}
+	LOG_DEBUG_WITH_TAG(response, "HttpResponse::generateErrorResponse");
+	return response;
+}
+
 HttpResponse::HttpResponse(HttpHeader &header, int fds) : header(header), fds(fds) {
+	LOG_DEBUG("HttpResponse::HttpResponse");
 	config = Config::getInstance();
 	response = "HTTP/1.1 ";
 	error = header.getError();
 	if (error.code() != 0)
 	{
-		response += header.getError().code() + " ";
-		response += header.getError().message() + "\r\n";
-		response += "Connection: close\r\n\r\n";
+		response = generateErrorResponse(error.code(), error.message());
 		return;
 	}
 
 	if (header.getMethod() == "GET")
 	{
 		if (config->isDirectiveAllowed(header.getPath(), header.getHeader("host"), Config::AllowedMethods, "GET"))
+		{
+			std::string filePath = config->getFilePath(header.getPath(), header.getHeader("host"));
+			getFile.open(filePath.c_str());
+			if (!getFile.is_open())
+				error = HttpError(404, "Not Found");
 			response += "transfer-encoding: chunked\r\n\r\n";
+		}
 		else
 			error = HttpError(405, "Method Not Allowed");
 	}
@@ -44,12 +78,7 @@ HttpResponse::HttpResponse(HttpHeader &header, int fds) : header(header), fds(fd
 		error = HttpError(405, "Method Not Allowed");
 	}
 	if (error.code() != 0)
-	{
-		config->getDirectiveValue(header.getPath(), header.getHeader("host"), Config::ErrorPage);
-		response += header.getError().code() + " ";
-		response += header.getError().message() + "\r\n";
-		response += "Connection: close\r\n\r\n";
-	}
+		response = generateErrorResponse(error.code(), error.message());
 }
 
 size_t HttpResponse::readBuffer(const char *buffer) {
@@ -60,17 +89,44 @@ size_t HttpResponse::readBuffer(const char *buffer) {
 }
 
 void HttpResponse::write() {
+	LOG_DEBUG("HttpResponse::write");
 	if (isFinished)
+	{
+		LOG_INFO("HttpResponse::write isFinished");
 		return;
+	}
 
-	if (error.code() != 0) {
-		size_t sentBytes =  (size_t) send(fds, response.c_str(), response.size(), 0);
+	if (error.code() != 0 || header.getMethod() == "DELETE"){
+		size_t sentBytes =  send(fds, response.c_str(), response.size(), 0);
 		if (sentBytes != response.size())
 			response = response.substr(sentBytes);
 		if (response.empty())
 			isFinished = true;
+	} else if (header.getMethod() == "GET") {
+		LOG_DEBUG("HttpResponse::sending GET response");
+		if (!response.empty())
+		{
+			size_t sentBytes =  (size_t) send(fds, response.c_str(), response.size(), 0);
+			if (sentBytes != response.size())
+				response = response.substr(sentBytes);
+		} else if (getFile.eof()) {
+			response = "0\r\n\r\n";
+			size_t sentBytes =  (size_t) send(fds, response.c_str(), response.size(), 0);
+			if (sentBytes != response.size())
+				response = response.substr(sentBytes);
+			if (response.empty())
+				isFinished = true;
+		} else {
+			getFile.read(ChunkedBuffer, 1024);
+			size_t readBytes = getFile.gcount();
+			response += readBytes;
+			response += "\r\n";
+			response += ChunkedBuffer;
+			response += "\r\n";
+		}
+
 	} else {
-		
+		LOG_ERROR("HttpResponse::IMPOSSIBLE STATE");
 	}
 }
 
