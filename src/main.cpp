f824b78196aff842b7ab5b136721fc661022a447
fbohling <fbohling@student.42wolfsburg.de>
2024-05-08 18:18:18 +0200

    issue: chunks not being read completely. function exists early if the chunkSize can be read because remaining is set to 0

--- src/main.cpp
+++ src/main.cpp
@@ -19,31 +19,83 @@
 
 UTEST_STATE();
 
+static size_t remaining = 0;
+
+static int decodeChunkedBody(std::string &bodyBuffer, std::string &decodedBody)
+{
+    if (bodyBuffer.empty() && remaining != 0) {
+        return -1; // Return -1 when an error occurs
+    }
+    std::stringstream bodyStream(bodyBuffer);
+    for (std::string line; remaining == 0 && std::getline(bodyStream, line);)
+    {
+        if (!line.empty() && line[line.size() - 1] == '\r')
+            line.erase(line.size() - 1); // Remove the trailing \r
+        std::stringstream sizeStream(line);
+        if (!(sizeStream >> std::hex >> remaining)) // Parse the chunk size
+            return -1; // Return -1 when an error occurs
+        if (remaining == 0)
+        {
+            // Check for final CRLF
+            char crlf[2];
+            if (!bodyStream.read(crlf, 2) || crlf[0] != '\r' || crlf[1] != '\n')
+                return -1; // Return -1 when an error occurs
+            return 0; // Return 0 when the entire body has been processed
+        }
+    }
+    if (remaining > 0)
+    {
+        size_t chunkSize = std::min(remaining, bodyBuffer.size());
+        size_t oldSize = decodedBody.size();
+        decodedBody.resize(oldSize + chunkSize); // Resize the decoded body buffer to fit the new chunk
+        if (!bodyStream.read(&decodedBody[oldSize], chunkSize)) // Write the chunk data to the decoded body buffer
+            return -1; // Return -1 when an error occurs
+        remaining -= chunkSize;
+        // Check for CRLF after chunk
+        std::cout << "Remaining: " << remaining << std::endl;
+        char crlf[2];
+        if (!bodyStream.read(crlf, 2) || crlf[0] != '\r' || crlf[1] != '\n')
+            return -1; // Return -1 when an error occurs
+    }
+    return 1; // Return 1 when a chunk has been processed but there are more chunks to process
+}
+
 int main(int argc, char *argv[], char *envp[]) {
 	if (argc != 2) {
 		std::cerr << "Usage: " << argv[0] << " <config file>" << std::endl;
 		return 1;
 	}
 	(void)envp;
-	Config *config = Config::getInstance();
-	try {
-		config->parseConfigFile(argv[1]);
-	} catch (std::exception &e) {
-		std::cerr << e.what() << std::endl;
-		return 1;
-	}
-	if (!config->isLoaded())
-		return 1;
-	config->printProgressBar(1, 1);
-	std::cout << GBOLD("\nConfig file loaded successfully") << std::endl;
+    std::string bodyBuffer = "4\r\nWiki\r\n5\r\npedia\r\nE\r\n in\r\n\r\nchunks.\r\n0\r\n\r\n";
+    std::string decodedBody;
+    for (size_t i = 0; i < bodyBuffer.size(); i += 10)
+    {
+        std::string chunk = bodyBuffer.substr(i, 10);
+        std::cout << "Return value: " << decodeChunkedBody(chunk, decodedBody) << std::endl;
+        std::cout << "Chunk: |" << chunk << "|" << std::endl;
+        std::cout << "Decoded body: |" << decodedBody << "|" << std::endl;
+        
+    }
+    std::cout << "Finished: " << decodedBody << std::endl;
+	// Config *config = Config::getInstance();
+	// try {
+	// 	config->parseConfigFile(argv[1]);
+	// } catch (std::exception &e) {
+	// 	std::cerr << e.what() << std::endl;
+	// 	return 1;
+	// }
+	// if (!config->isLoaded())
+	// 	return 1;
+	// config->printProgressBar(1, 1);
+	// std::cout << GBOLD("\nConfig file loaded successfully") << std::endl;
 
-	LOG_INFO("Server started");
-	// LOG_SET_LOG_LEVEL(Logging::DISABLE_LOG);
-	// LOG_DISABLE_CONSOLE_LOGGING();
-	// LOG_SET_LOG_TARGET(Logging::LOG_TO_FILE);
-	SocketHandling sockets(config->getServerBlocks());
-	EventHandler event(sockets);
-	event.start();
+	// LOG_INFO("Server started");
+	// // LOG_SET_LOG_LEVEL(Logging::DISABLE_LOG);
+	// // LOG_DISABLE_CONSOLE_LOGGING();
+	// // LOG_SET_LOG_TARGET(Logging::LOG_TO_FILE);
+	// SocketHandling sockets(config->getServerBlocks());
+	// EventHandler event(sockets);
+	// event.start();
 }
 
 // int main()
