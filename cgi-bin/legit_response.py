8c387c27fe0ed3c93a0407e1ee8c96c0a95b53db
stevebalk <steve.balk@web.de>
2024-05-11 20:54:33 +0200

    Changed directory name

---
+++ cgi-bin/legit_response.py
@@ -0,0 +1,6 @@
+print("HTTP/1.0 200 OK")
+print("Content-Type: text/html")    # HTML is following
+print()                             # blank line, end of headers
+print("<TITLE>CGI script output</TITLE>")
+print("<H1>This is my first CGI script</H1>")
+print("Hello, world!")
