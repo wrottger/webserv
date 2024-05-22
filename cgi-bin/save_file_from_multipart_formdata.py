6875cb01ce025fbe400d15f063af76fbc0196884
dnebatz <dnebatz@student.42wolfsburg.de>
2024-05-22 11:01:16 +0200

    fixed upload script

--- cgi-bin/save_file_from_multipart_formdata.py
+++ cgi-bin/save_file_from_multipart_formdata.py
@@ -1,48 +1,38 @@
-#!/usr/bin/env python3
-
 import cgi
 import os
-import cgitb
-cgitb.enable()
 
 form = cgi.FieldStorage()
+print("HTTP/1.0 200 OK")
+print("Content-type: text/html")
+print()
+print('<html>')
+print('<body>')
+file_uploaded = False
+save_path = "/home/dnebatz/Documents/realwebserv/webserv/www/"  # specify your save path here
 
-# Directory to save files
-save_path = '/home/dnebatz/Documents/realwebserv/webserv/www/'
-
-# Go through all fields in the form
+# Iterate over all fields in the form
 for key in form.keys():
-	fileitem = form[key]
+    fileitem = form[key]
 
-	# Check if the field is a list of files
-	if isinstance(fileitem, list):
-		for item in fileitem:
-			if item.filename:
-				# Strip leading path from file name to avoid directory traversal attacks
-				fn = os.path.basename(item.filename)
-				with open(save_path + fn, 'wb') as f:
-					chunk = item.file.read(8192)
-					while chunk:
-						f.write(chunk)
-						chunk = item.file.read(8192)
+    # Check if the field is a list
+    if isinstance(fileitem, list):
+        # Process each item in the list
+        for item in fileitem:
+            if item.filename:
+                # Debug print: print the filename of the uploaded file
+                print(f'<p>Uploaded file: {item.filename}</p>')
+                # Strip leading path from file name to avoid directory traversal attacks
+                fn = os.path.basename(item.filename)
+                open(os.path.join(save_path, fn), 'wb').write(item.file.read())
+                print(f'<p>File "{fn}" was uploaded successfully</p>')
+    else:
+        if fileitem.filename:
+            # Debug print: print the filename of the uploaded file
+            print(f'<p>Uploaded file: {fileitem.filename}</p>')
+            # Strip leading path from file name to avoid directory traversal attacks
+            fn = os.path.basename(fileitem.filename)
+            open(os.path.join(save_path, fn), 'wb').write(fileitem.file.read())
+            print(f'<p>File "{fn}" was uploaded successfully</p>')
 
-				print(f'The file "{fn}" was uploaded successfully<br>')
-	else:
-		# Check if the field is a file
-		if fileitem.filename:
-			# Strip leading path from file name to avoid directory traversal attacks
-			fn = os.path.basename(fileitem.filename)
-			with open(save_path + fn, 'wb') as f:
-				chunk = fileitem.file.read(8192)
-				while chunk:
-					f.write(chunk)
-					chunk = fileitem.file.read(8192)
-
-			print(f'The file "{fn}" was uploaded successfully<br>')
-
-print("""\
-Content-Type: text/html\n
-<html><body>
-<p>All files uploaded successfully</p>
-</body></html>
-""")
+print('</body>')
+print('</html>')
