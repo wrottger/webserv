19b4b6b1da85391d24348c3ecbfba31bbab309b7
dnebatz <dnebatz@student.42wolfsburg.de>
2024-05-22 14:21:35 +0200

    added scripts

---
+++ cgi-bin/delete.php
@@ -0,0 +1,40 @@
+HTTP/1.1 200 OK
+Content-type: text/html
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Info</title>
+</head>
+<body>
+	<h1>PHP Info</h1>
+	<?php
+		// Get all files in the current directory
+		$files = glob('*');
+
+		// Display each file with a delete button
+		foreach ($files as $file) {
+			if (is_file($file)) {
+				$functionName = 'sendDeleteRequest' . md5($file);
+				echo "
+				<p>$file</p>
+				<button onclick=\"$functionName()\">Send DELETE Request</button>
+
+				<script>
+				function $functionName() {
+					fetch('$file', {
+						method: 'DELETE',
+					})
+					.then(response => response.text())
+					.then(data => {
+						console.log(data);
+						window.location.reload(); // Refresh the page
+					})
+					.catch((error) => console.error('Error:', error));
+				}
+				</script>";
+			}
+		}
+	?>
+</body>
+</html>
