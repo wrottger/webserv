257a57b7c35d338060ac7c325735733580f67a25
dnebatz <dnebatz@student.42wolfsburg.de>
2024-05-27 20:34:54 +0200

    added scripts for testing external redirects

---
+++ cgi-bin/externalredirectwithoutbody.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import cgi
+import cgitb
+
+cgitb.enable()  # for troubleshooting
+
+print("Location: https://www.hellabrunn.de/tiere/welt-der-affen")
+print() # to end the CGI response headers
